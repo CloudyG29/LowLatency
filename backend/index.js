9b9c1f6be099d03e104087e0bc0255400369fa81
Mohlala Kgaogelo <Kgaogelog0@gmail.com>
2026-04-18 01:47:01 +0200

    created get_user , which will be the foundation for removing stubs

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
-const cors = require('cors');
-const userRoutes = require('./routes/user');
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,14 +10,17 @@ app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.use(cors());
 app.use(express.json());
+const userRoutes = require("./routes/user");
+const adminRoutes = require("./routes/get_user");
 
+app.use("/api/admin", adminRoutes);
 // Fix COOP header to allow Firebase popups
 app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
   next();
 });
 
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend", "index.html"));
@@ -29,11 +31,15 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/signup-applicant", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/views", "signup-applicant.html"));
+  res.sendFile(
+    path.join(__dirname, "../frontend/views", "signup-applicant.html"),
+  );
 });
 
 app.get("/signup-provider", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/views", "signup-provider.html"));
+  res.sendFile(
+    path.join(__dirname, "../frontend/views", "signup-provider.html"),
+  );
 });
 
 app.get("/signup-admin", (req, res) => {
@@ -41,19 +47,27 @@ app.get("/signup-admin", (req, res) => {
 });
 
 app.get("/applicant", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/roles_htmls", "applicant_view.html"));
+  res.sendFile(
+    path.join(__dirname, "../frontend/roles_htmls", "applicant_view.html"),
+  );
 });
 
 app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/roles_htmls", "admin_view.html"));
+  res.sendFile(
+    path.join(__dirname, "../frontend/roles_htmls", "admin_view.html"),
+  );
 });
 
 app.get("/provider", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/roles_htmls", "provider_view.html"));
+  res.sendFile(
+    path.join(__dirname, "../frontend/roles_htmls", "provider_view.html"),
+  );
 });
 
 app.get("/forgot-password", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/views", "forgot-password.html"));
+  res.sendFile(
+    path.join(__dirname, "../frontend/views", "forgot-password.html"),
+  );
 });
 
 module.exports = app;
