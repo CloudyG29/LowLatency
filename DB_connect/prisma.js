3ecde47030fb920f6b7cf23073591ccd08a48d3b
Nduvho Mulaudzi <ngmulaudzi2@gmail.com>
2026-05-09 19:50:14 +0200

    fix/changed prisma location and config

--- DB_connect/prisma.js
+++ DB_connect/prisma.js
@@ -1,9 +1,8 @@
-require("dotenv").config();
-const { PrismaClient } = require("@prisma/client");
-const { PrismaMssql } = require("@prisma/adapter-mssql");
+require('dotenv').config();
+const { PrismaClient } = require('../generated/client');
+const { PrismaMssql } = require('@prisma/adapter-mssql');
 
 const adapter = new PrismaMssql(process.env.DATABASE_URL);
 const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
